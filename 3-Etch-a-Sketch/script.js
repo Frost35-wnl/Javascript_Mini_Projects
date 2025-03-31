a31539e2a6e95d71a2bf752a76e788298294f417
Voary Luciano <137229836+VoaryLuciano@users.noreply.github.com>
2025-03-31 21:23:03 +0000

    Add Etch-a-Sketch project

---
+++ 3-Etch-a-Sketch/script.js
@@ -0,0 +1,60 @@
+const root = document.documentElement;
+let containerDiv = document.querySelector(".container");
+let startButton = document.querySelector("button");
+let userDimension;
+
+//main
+startButton.addEventListener('click', () => {
+    removeGrid();
+    userDimension = takeUserDimension();
+    root.style.setProperty('--user-dimension', userDimension);
+
+    createGrid(userDimension);
+    //startButton.style.display = 'none';
+});
+
+
+function takeUserDimension() {
+    let userInput;
+
+    do {
+        if(userInput > 100 || userInput < 1) alert("The value must be between 1 and 100");
+        userInput = prompt("Please enter the number of squares in the width and height", 100);
+    } while (userInput > 100 || userInput < 1);
+
+    return userInput;
+}
+
+function createGrid(dimension)
+{
+    let opacityDensity = 10; 
+
+    for (let index = 1; index <= (dimension * dimension); index++) {
+        let redValue = Math.random()*255;
+        let greenValue = Math.random()*255;
+        let blueValue = Math.random()*255;
+
+        const div = document.createElement("div");
+        div.classList.add("new");
+        div.style.opacity = opacityDensity + '%';
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')';
+            div.style.opacity = (opacityDensity += 10) + '%';
+        } );
+        containerDiv.appendChild(div);
+    }
+   
+}
+
+function removeGrid(){
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.lastChild);   
+    }
+}
+
+
+
+
+
+
